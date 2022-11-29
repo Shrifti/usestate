3b321eb3772164719b5f1f2298978b48f8eda61b
Sharif Tibi <shariftibi@gmail.com>
2022-11-29 11:47:54 +0200

    Max number

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css'
 import Counter from './Counter';
-
+import { useState } from 'react';
 function App() {
+
+  const [delta, setDelta] = useState(1)
+  const [Maxnum, setMax] = useState(1)
+  function handleDelta(e){
+  console.log(e);  
+  setDelta(Number(e.target.value))
+}
+function handleMaxnum(maxnumber){
+  setMax(Number(maxnumber.target.value))
+}
   return (
     <div className="App">
       Usestate
-      <p>Another Line</p>
-      <Counter/>
+      <input type = "number" value ={delta} onChange = {handleDelta}/>
+      <input type = "number" value ={Maxnum} onChange = {handleMaxnum}/>
+      <Counter delta = {delta} maxnum = {Maxnum}/>
+      <Counter delta = {delta} maxnum ={Maxnum}/>
     </div>
   );
 }
